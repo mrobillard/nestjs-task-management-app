6be534ac69d69c911c71a1dd825ab0dd0b1d7f65
Matthew Robillard <robillard.matt@gmail.com>
2020-03-01 13:19:19 -0500

    Add JWT authorization.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule],
+    TasksModule,
+    AuthModule],
 })
 export class AppModule {}
